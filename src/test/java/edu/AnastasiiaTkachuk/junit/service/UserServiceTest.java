a6c0fa53167969404be89e771f5fe1c67c908d00
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-03-05 20:09:57 +0200

    add ParameterizedTest and DI

--- src/test/java/edu/AnastasiiaTkachuk/junit/service/UserServiceTest.java
+++ src/test/java/edu/AnastasiiaTkachuk/junit/service/UserServiceTest.java
@@ -1,31 +1,42 @@
 package edu.AnastasiiaTkachuk.junit.service;
 
 import edu.AnastasiiaTkachuk.junit.dto.User;
+import edu.AnastasiiaTkachuk.junit.paramresolver.UserServiceParamResolver;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
 @Tag("fast")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith({
+        UserServiceParamResolver.class
+})
 public class UserServiceTest {
     private UserService userService;
     private static final User IVAN = User.of(1, "Ivan", "123");
     private static final User PETR = User.of(2, "Petr", "111");
 
+    UserServiceTest(TestInfo testInfo){
+        System.out.println();
+    }
     @BeforeAll
     void init(){
         System.out.println("Before all: " + this);
     }
 
     @BeforeEach
-    void prepare(){
+    void prepare(UserService userService){
         System.out.println("Before each: " + this);
-        userService = new UserService();
+        this.userService = userService;
     }
 
     @Test
@@ -44,25 +55,6 @@ public class UserServiceTest {
     }
 
     @Test
-    @Tag("login")
-    void loginSuccessIfUserExists(){
-        userService.add(IVAN);
-        Optional<User> maybeUser = userService.login(IVAN.getUsername(), IVAN.getPassword());
-        assertThat(maybeUser).isPresent();
-        maybeUser.ifPresent(user -> assertThat(user).isEqualTo(IVAN));
-    }
-    @Test
-    @Tag("login")
-    void throwExceptionIfUsernameOrPasswordIsNull(){
-        assertAll(
-                () -> assertThrows(IllegalArgumentException.class,
-                        () -> userService.login(null, "1234")),
-                () -> assertThrows(IllegalArgumentException.class,
-                        () -> userService.login("Ivan", null))
-        );
-    }
-
-    @Test
     void usersConvertedToMapById(){
         userService.add(IVAN, PETR);
         Map<Integer, User> users = userService.getAllConvertedById();
@@ -71,21 +63,35 @@ public class UserServiceTest {
                 () -> assertThat(users).containsValues(IVAN, PETR)
         );
     }
-    @Test
-    @Tag("login")
-    void loginFailIfPasswordIsNotCorrect(){
-        userService.add(IVAN);
-        Optional<User> maybeUser = userService.login(IVAN.getUsername(), "jdfjkfk");
-        assertThat(maybeUser).isEmpty();
-    }
-    @Test
-    @Tag("login")
-    void loginFailIfUserDoesNotExist(){
-        userService.add(IVAN);
-        Optional<User> maybeUser = userService.login("Anastasiia", IVAN.getPassword());
-        assertThat(maybeUser).isEmpty();
-    }
 
+    @ParameterizedTest
+//    @ArgumentsSource()
+//    @NullSource
+//    @EmptySource
+//    @ValueSource(strings = {
+//            "Ivan", "Petr"
+//    })
+//    @EnumSource
+//    @NullAndEmptySource
+    @MethodSource("getArgumentsForLoginTest")
+//    @CsvFileSource(resources = "/login-test-data.csv", numLinesToSkip = 1)
+//    @CsvSource({
+//            "Ivan", "123",
+//            "Petr", "111"
+//    })
+    void loginParametrizedTest(String username, String password, Optional<User> user){
+        userService.add(IVAN, PETR);
+        Optional<User> maybeUser = userService.login(username, password);
+        assertThat(maybeUser).isEqualTo(user);
+    }
+    static Stream<Arguments> getArgumentsForLoginTest(){
+        return Stream.of(
+                Arguments.of("Ivan", "123", Optional.of(IVAN)),
+                Arguments.of("Petr", "111", Optional.of(PETR)),
+                Arguments.of("Ivan", "sfdfsd", Optional.empty()),
+                Arguments.of("sdsf", "123", Optional.empty())
+        );
+    }
     @AfterEach
     void deleteDataFromDatabase(){
         System.out.println("After each: " + this);
@@ -94,5 +100,38 @@ public class UserServiceTest {
     @AfterAll
     void closeConnectionPool(){
         System.out.println("After all: " + this);
+    }
+
+    @Nested
+    @Tag("login")
+    class LoginTest {
+        @Test
+        void loginFailIfPasswordIsNotCorrect(){
+            userService.add(IVAN);
+            Optional<User> maybeUser = userService.login(IVAN.getUsername(), "jdfjkfk");
+            assertThat(maybeUser).isEmpty();
+        }
+        @Test
+        void loginFailIfUserDoesNotExist(){
+            userService.add(IVAN);
+            Optional<User> maybeUser = userService.login("Anastasiia", IVAN.getPassword());
+            assertThat(maybeUser).isEmpty();
+        }
+        @Test
+        void loginSuccessIfUserExists(){
+            userService.add(IVAN);
+            Optional<User> maybeUser = userService.login(IVAN.getUsername(), IVAN.getPassword());
+            assertThat(maybeUser).isPresent();
+            maybeUser.ifPresent(user -> assertThat(user).isEqualTo(IVAN));
+        }
+        @Test
+        void throwExceptionIfUsernameOrPasswordIsNull(){
+            assertAll(
+                    () -> assertThrows(IllegalArgumentException.class,
+                            () -> userService.login(null, "1234")),
+                    () -> assertThrows(IllegalArgumentException.class,
+                            () -> userService.login("Ivan", null))
+            );
+        }
     }
 }
